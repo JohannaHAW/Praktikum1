13492f24c492877c6c99a8c3775d19b0bc014ccd
Your Name <you@example.com>
2015-10-30 14:07:16 +0100

    probieren

--- PM2/src/aufgabenblatt2/DoubleDoubleZuDouble.java
+++ PM2/src/aufgabenblatt2/DoubleDoubleZuDouble.java
@@ -6,7 +6,7 @@ package aufgabenblatt2;
  * @author Thi Huyen, Cao(Lilli) email: ThiHuyen.Cao@haw-hamburg.de Johanna Ahlf
  *         email: johanna.ahlf@haw-hamburg.de Technische Informatik, HAW Prof.
  *         Philipp Jenke
- */
+ *johnannanannanan*/
 @FunctionalInterface
 public interface DoubleDoubleZuDouble {
 	public double werteAus(double wert1, double wert2);
